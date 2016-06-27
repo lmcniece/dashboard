ea86be7536107dd26230234af610a528b115fb9e
x3n7r0py <logan.mcniece@gmail.com>
2016-06-27 09:34:23 -0500

    fixes issue with ACS/ECSI loans coalescing instead of summing

--- services/sql/loans.sql
+++ services/sql/loans.sql
@@ -1,14 +1,11 @@
-WITH total AS(
-	select 'TOTAL' as account, sum(outstanding) as ttl_out from loans where outstanding > 0
-)
-
 (SELECT 
 	upper(acct) as account,
-	round(original,2) as principle,
-	round(outstanding,2) as outstanding,
+	round(sum(original),2) as principle,
+	round(sum(outstanding),2) as outstanding,
 	round(rate * 100,2) || '%' as rate
 FROM loans
 WHERE outstanding > 0
+GROUP BY 1,4
 )UNION(
 SELECT 
 	'TOTAL' as account,
@@ -16,7 +13,7 @@ SELECT
 	round(sum(outstanding),2) as outstanding,
 	round(sum((outstanding/ttl_out)*rate*100),2)||'%' as rate
 FROM loans
-CROSS JOIN (select ttl_out from total) subqy
+CROSS JOIN (select sum(outstanding) ttl_out from loans) subqy
 WHERE outstanding > 0
 )
 ORDER BY outstanding
