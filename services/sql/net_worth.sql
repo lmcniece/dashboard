3f71575a70bd8611a53f503eee67d185263c20fe
x3n7r0py <logan.mcniece@gmail.com>
2016-09-16 08:10:10 -0500

    update net worth sql to include hsa and floating cash

--- services/sql/net_worth.sql
+++ services/sql/net_worth.sql
@@ -1,4 +1,5 @@
-SELECT trunc(performance.val - loans.val,2) AS net_worth 
+SELECT trunc(performance.val - loans.val + hsa.val + 1000,2) AS net_worth 
 FROM 
-	(SELECT sum(invested + roi) AS val FROM performance WHERE holder = 'logan') AS performance, 
-	(SELECT sum(outstanding) AS val FROM loans) AS loans
+	(SELECT sum(invested + roi) as val FROM performance WHERE holder = 'logan') AS performance, 
+	(SELECT sum(outstanding) as val FROM loans) AS loans,
+	(SELECT amount as val FROM cashflow WHERE account = 'hsa') AS hsa
