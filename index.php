71547dfe024861636126b6e854d591faa8e4afe8
Logan McNiece <logan@sproutsocial.com>
2017-11-27 21:30:06 -0600

    refactors countdown to Lyssa

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<script src="assets/lib/jquery-2.1.4.min.js"></script>
 		<script src="assets/lib/jquery-ui.js"></script>
 		<script src="assets/lib/bootstrap.js"></script>
-		<script src="js/global_vars_funcs.js"></script>
+		<script src="assets/js/global_vars_funcs.js"></script>
 	</head>
 	<style>
 		body{
@@ -34,8 +34,8 @@
 			margin-right:auto;
 			margin-left:auto;
 		}
-		#quote-container{
-			width:50%;
+		#timer{
+			font-size:100px;
 		}
 		#jona_lisa{
 			height:400px;
@@ -61,18 +61,7 @@
 		}
 	</style>
 	<body>
-		<div id="king_queen-container" class="container">
-			<img id="archon" src="assets/img/archon.jpg">
-			<img id="jona_lisa" src="assets/img/jona_lisa.jpg">
-		</div>
-		<div id="quote-container" class="container">
-			<h3 style="text-align:left">
-				"Thou must persist through forty days and forty nights, forty suns and forty moons. If thou doest this for me, heaven's gate will open unto thee and basketballs will fall from the sky." 
-			</h3>
-			<h5 style="text-align:right">
-				~ Third Book of the Revelations of Jona Lisa 17:9
-			</h5>
-		</div>
+		<div id="timer" class="container"></div>
 		<div id="advent-container" class="container"></div>
 		<div id="image_overlay-container" class="container"></div>
 		<div id="overlay"></div>
@@ -80,12 +69,41 @@
 </html>
 <script>
 
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 15, 2017 10:30:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "DADDY IS HERE!";
+  }
+}, 1000);
+
 function openDays(){
-	var endtime = "Sat Mar 17 2017 00:00:00 GMT-0600 (Central Standard Time)";
-	var t = Date.parse(endtime) - Date.parse(new Date());
+	var endtime = new Date("Dec 15, 2017 10:30:25").getTime();
+	var t = endtime - Date.parse(new Date());
 	var days_remaining = Math.floor(t / (1000 * 60 * 60 * 24));
 	for(var i=days_remaining;i<41;i++){
-		$("#cell-"+i).css("background","#f00");
+		$("#cell-"+i).css("background","#aaa");
 		$("#cell-"+i).addClass("opened_day");
 		$("#cell-"+i).html('<img class="advent-image" src="assets/img/advent-imgs/'+i+'.gif">');
 	}
@@ -107,7 +125,7 @@ function generateTable(rows, columns, container){
 	for(var i=0;i<rows;i++){
 		table += '<tr>';
 		for(var ii=0;ii<columns;ii++){
-			var image_num = String(40-(i*columns+ii));
+			var image_num = String((rows*columns)-(i*columns+ii));
 			table += '<td id="cell-'+image_num+'" image_num="'+image_num+'">'+image_num+'</td>'
 		}
 		table += '</tr>';
@@ -124,8 +142,8 @@ function calculateCSS(columns){
 }
 
 $("#overlay").css("display","none");
-generateTable(5,8,$("#advent-container"));
-calculateCSS(8);
+generateTable(7,3,$("#advent-container"));
+calculateCSS(3);
 openDays();
 
 </script>
